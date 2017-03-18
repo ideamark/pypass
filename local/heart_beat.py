23bb0098641c5df3818ac3c29580ad2c4c14eeb6
Mark Young <ideamark@qq.com>
2017-03-18 08:21:04 +0800

    modified:   heart_beat.py

--- local/heart_beat.py
+++ local/heart_beat.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import socket, sys, time
 
-def base():
-    HOST = '123.57.230.28'
-    PORT = 2010
+def heart_beat():
+    HOST = '' # Your remote server IP
+    PORT = 2010 # Your remote server port
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
         port = int(PORT)
@@ -20,17 +20,10 @@ def base():
                 break
             elif buf == b'#OK':
                 print('Recive OK')
-            time.sleep(2) # Set your heart beat delay, 1 min is recommended.
+            time.sleep(60) # Set your heart beat delay, 1 min is recommended.
         except KeyboardInterrupt:
             sys.exit(1)
 
-def start():
-    import threading
-    t = threading.Thread(target=base)
-    t.setDaemon(True)
-    t.start()
-    print('Heart beat started')
-
 if __name__ == '__main__':
-    start()
-    while True: pass
+    print('Start heart beat...')
+    heart_beat()
